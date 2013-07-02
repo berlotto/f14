e4f5fe8c12864f79dc00f9bc0c452456146d6f32
Sérgio Berlotto Jr <sergio@r2d2.(none)>
2013-07-02 01:30:57 -0300

    Primeira versão utilizável

---
+++ static/slide_config.js
@@ -0,0 +1,41 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'Flask<br>Faça tudo o que quiser!',
+    subtitle: 'Um grande micro-framework Python para web.',
+    eventInfo: {
+      title: 'FISL-14',
+      date: '3 a 6 de julho/2013'
+    },
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+    favIcon: 'images/fav-icon.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ],
+    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Sérgio Berlotto Jr',
+    company: 'Desenvolvedor Python<br>Gabinete Digital - SGG/RS',
+    // gplus: 'https://plus.google.com/107402910308625921381',
+    facebook: 'sergio.berlotto',
+    twitter: '@berlottocdd',
+    www: 'http://pythonrs.org',
+    github: 'http://github.com/berlotto'
+  }/*, {
+    name: 'Second Name',
+    company: 'Job Title, Google',
+    gplus: 'http://plus.google.com/1234567890',
+    twitter: '@yourhandle',
+    www: 'http://www.you.com',
+    github: 'http://github.com/you'
+  }*/]
+};
+
