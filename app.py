ca6818ab20312e972448e192a98cc0d152204787
Sérgio Berlotto <sergio.berlotto@gmail.com>
2013-07-06 01:40:16 -0300

    Heroku adjusts (2)

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # -*- encoding: utf-8 -*-
 
 from flask import Flask, render_template, request
+import os
 
 app = Flask(__name__)
 
@@ -9,4 +10,5 @@ def index():
     return render_template('template.html')
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get('PORT',5000))
+    app.run(host='0.0.0.0', port=port)
